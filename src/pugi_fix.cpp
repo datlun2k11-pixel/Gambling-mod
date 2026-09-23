5ad047c9e606a0232767189985f2a9d3dea4c4e0
datlun2k11-pixel <datlun2k11@gmail.com>
2026-09-23 20:17:15 +0700

    Try asm label for Windows dtor

--- src/pugi_fix.cpp
+++ src/pugi_fix.cpp
@@ -1,13 +1,14 @@
 // Dummy fix for pugi::xml_node destructor linker error
-// Provide symbols without including headers to avoid redefinition
 
 #if defined(_WIN32) || defined(_MSC_VER) || defined(GEODE_IS_WINDOWS)
-// MSVC mangling for pugi::xml_node::~xml_node is ??1xml_node@pugi@@QEAA@XZ
-// Use inline assembly to define it without C++ redefinition
-__asm__(".globl \"??1xml_node@pugi@@QEAA@XZ\"\n\"??1xml_node@pugi@@QEAA@XZ\":\n\tret\n");
+// MSVC mangling: provide symbol via asm label
+extern "C" void win_pugi_dtor() asm("??1xml_node@pugi@@QEAA@XZ");
+void win_pugi_dtor() {}
+// Also provide alternative mangling that lld might look for
+extern "C" void win_pugi_dtor2() asm("??1xml_node@pugi@@QAE@XZ");
+void win_pugi_dtor2() {}
 void pugi_fix_dummy_win() {}
 #else
-// Itanium ABI (Android/macOS/iOS/Linux) - use extern C which works for these
 extern "C" void _ZN4pugi8xml_nodeD1Ev(void* self) {}
 extern "C" void _ZN4pugi8xml_nodeD2Ev(void* self) {}
 #endif
